df3cd67dfb80246b821d93e83f7043e50bbc5f58
chinenye997 <edehg997@gmail.com>
2024-12-18 15:45:19 +0100

    fixt push issue

---
+++ Bluetagtask2of2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluetagtask2of2
+{
+
+    /*PythagoreanTheorem Theorem = new PythagoreanTheorem();
+    Theorem.*/
+    class PythagoreanTheorem
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter side A:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side C:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+
+            double d = Math.Sqrt((a * a) + (b * b) + (c * c));
+
+            Console.WriteLine("The hypotenuse is:" + d);
+
+            Console.ReadLine();
+        }
+    }
+}
